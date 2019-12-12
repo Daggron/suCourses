0b9f84ff5bf0bae79379a44690bc3875e415ec8a
Daggron <returnofking04@gmail.com>
2019-12-13 00:41:15 +0530

    Added the search endpoint

--- server/api/controllers/courses.controller.js
+++ server/api/controllers/courses.controller.js
@@ -43,39 +43,27 @@ exports.post = async (req,res)=>{
 
 exports.search = async(req,res)=>{
     console.log(req.params.search)
-    const regex = new RegExp(escapeRegex(req.params.search));
-    let courses = [];
-    const raw = await getAsync(JSON.stringify(regex));
-    const data = await JSON.parse(raw);
-    if(data&&data.length!==0){
-        const course = [...data];
-        return res.json({
-            data:course
-        })
-    }
-    else{
-        const raw = await JSON.parse(await getAsync('courses'));
-        const response = [...raw];
-        const courses = response.filter(eachresponse=>{
-            console.log('sec')
-            if(
-                (eachresponse.title.toLowerCase().includes(req.params.search))
-                ||
-                (eachresponse.Category.toLowerCase().includes(req.params.search))
-                ||
-                (eachresponse.instructor.toLowerCase().includes(req.params.search))
-            ){
-                return eachresponse;
-            }
-            else{
-                return false;
-            }
-        })
-        const success =await  setAsync(JSON.stringify(regex),JSON.stringify(courses));
-        res.json({
-            data:courses
-        })
-    }
+    const raw = JSON.parse(await getAsync('courses'));
+    const data = [...raw];
+    const  courses = raw.filter((eachCourse)=>{
+        const title =  eachCourse.title.toLowerCase().replace(/\s/g,'');
+        const instructor = eachCourse.instructor.toLowerCase().replace(/\s/g,'');
+        const Category = eachCourse.Category.toLowerCase().replace(/\s/g,'');
+        if(
+            title.includes(req.params.search.toLowerCase().replace(/\s/g,''))
+            ||
+            instructor.includes(req.params.search.toLowerCase().replace(/\s/g,''))
+            ||
+            Category.includes(req.params.search.toLowerCase().replace(/\s/g,''))
+        ){
+            return eachCourse;
+        }
+        return false;
+    })
+    // console.log(courses);
+    return res.json({
+        data:courses
+    })
     
 }
 
