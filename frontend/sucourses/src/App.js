5f3efc4a557e9290cd106d0a953d91e3cdf6f06e
Daggron <returnofking04@gmail.com>
2019-12-15 14:56:09 +0530

    Added more categories and implemented styling

--- frontend/sucourses/src/App.js
+++ frontend/sucourses/src/App.js
@@ -6,19 +6,36 @@ import Home from './components/home';
 import Categories from './components/category/categories';
 import Courses from './components/courses/courses';
 import Allcourses from './components/courses/all-courses';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { ThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: white)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <div className="App">
-        <Router>
-           <Drawer />
-            <Route exact path="/" component={Home}/>
-            <Route  path="/course"  component={Allcourses}/>
-            <Route exact path="/category" component={Categories} />
-            <Route path="/category/find" component={Courses} />
-           
-        </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+          <Router>
+            <Drawer />
+              <Route exact path="/" component={Home}/>
+              <Route  path="/course"  component={Allcourses}/>
+              <Route exact path="/category" component={Categories} />
+              <Route path="/category/find" component={Courses} />
+            
+          </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
