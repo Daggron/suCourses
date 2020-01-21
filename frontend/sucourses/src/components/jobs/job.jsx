4d584b621d08a05e3b0104fcd97de2f10fbf844a
Daggron <returnofking04@gmail.com>
2020-01-22 00:57:33 +0530

    Added Jobs

--- frontend/sucourses/src/components/jobs/job.jsx
+++ frontend/sucourses/src/components/jobs/job.jsx
@@ -1,26 +1,49 @@
 import React from 'react'
 import { Card, CardContent, Typography } from '@material-ui/core'
+import AlertDialogSlide from './dialoug';
+import {Button} from '@material-ui/core'
 
 export default function Job(props) {
+    const [open, setOpen] = React.useState(false);
+    const [oneJob , setJob] = React.useState({});
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    const selectJob = e =>{
+        setJob(e);
+    }
     return (
         <React.Fragment>
-            <Card style={{width : 300 , margin : 15 , height : 400}}>
+            <AlertDialogSlide job={oneJob} open={open} handleClickOpen={handleClickOpen} handleClose={handleClose} />
+            <Card style={{width : 300 , margin : 15 , height : 230}}>
                 <CardContent>
-                    <Typography>
+                    <Typography style={{marginTop : 15}}>
                        Title : {
                             props.job.title
                         }
                     </Typography>
-                    <Typography>
+                    <Typography style={{marginTop : 15}}>
                         Company : {
                             props.job.company
                         }
                     </Typography>
-                    <Typography>
+                    <Typography style={{marginTop : 15}}>
                         Type : {
                             props.job.type
                         }
                     </Typography>
+                    <Button variant="contained" color="primary" style={{marginTop : 10}} onClick={()=>{
+                        setJob(props.job);
+                        setOpen(true)
+                    }}>
+                        View
+                    </Button>
                 </CardContent>
             </Card>
         </React.Fragment>
